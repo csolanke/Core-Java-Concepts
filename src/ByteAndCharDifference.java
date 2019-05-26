295df0716d463fc3eae72d1317949500811b06c9
Chandrakant <csolanke77@gmail.com>
2019-05-26 12:04:14 +0530

    1. difference between char and Byte

---
+++ src/ByteAndCharDifference.java
@@ -0,0 +1,32 @@
+
+public class ByteAndCharDifference {
+	
+	public static void main(String[] args) {
+		
+		// Both char and byte are used to accept numeric data type;
+	
+		char a = 65;
+		
+		//this will print the alphabet whose ascii value is 65;
+		System.out.println(a);
+		
+		
+		
+		// if we assign single character to the char type it will print the same char value
+		char c = 'b';
+		System.out.println(c);
+		
+		// Byte data type is used to store numneric value;
+		
+	     Byte b = 78;
+	     
+	     System.out.println(b);
+		
+		// also we can store the single alphabets in byte type as well.
+	     // if we do so it will print numeric value of that char
+	     Byte y = 'A';
+	     System.out.println(y);
+		
+	}
+
+}
